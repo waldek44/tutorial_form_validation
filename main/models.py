e030718abe66381dee53cae601b888e6a18dad3a
Waldek <reclikmail@gmail.com>
2019-12-31 10:18:12 +0100

    Commit 2 Model danych

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+# model named Post
+class Post(models.Model):
+    Male = 'M'
+    FeMale = 'F'
+    GENDER_CHOICES = (
+        (Male, 'Mężczyzna'),
+        (FeMale, 'Kobieta'),
+    )
+
+    username = models.CharField(max_length=20, blank=False, null=False)
+    text = models.TextField(blank=False, null=False)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, default=Male)
+    time = models.DateTimeField(auto_now_add=True)
