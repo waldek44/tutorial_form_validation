928d6936d4f6fb7078a788a12abe8e6472234fc5
Waldek <reclikmail@gmail.com>
2019-12-31 09:55:10 +0100

    start

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\recli\Desktop\DOJO\tutorial_form_validation\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
